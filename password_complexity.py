088d02a74b9c0cd359a8dbe2521081145437b669
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-18 16:32:26 +0000

    Create password_complexity.py

---
+++ password_complexity.py
@@ -0,0 +1,5 @@
+def check_password_complexity(password):
+    # Implement logic to check if the provided password meets the complexity requirements
+    # You may need to analyze the OAuth server's password policy to define the checks
+    # Example: Check for minimum length, uppercase, lowercase, digits, special characters, etc.
+    return True  # Return True if the password meets complexity requirements, else False
