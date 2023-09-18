0eca52876195349cfc91138616fbf97c452e120b
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-18 16:32:42 +0000

    Create captcha_handling.py

---
+++ captcha_handling.py
@@ -0,0 +1,14 @@
+import requests
+
+def check_captcha_challenge(token_endpoint):
+    try:
+        # Send a GET request to the token endpoint to check for CAPTCHA challenge
+        response = requests.get(token_endpoint, timeout=5)
+
+        if "captcha" in response.text.lower():
+            return True  # CAPTCHA challenge detected
+        else:
+            return False  # No CAPTCHA challenge detected
+    except requests.exceptions.RequestException as e:
+        print(f"CAPTCHA check failed with an exception: {str(e)}")
+        return False  # Error occurred, assume no CAPTCHA challenge
